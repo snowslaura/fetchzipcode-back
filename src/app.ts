98b1992db648a55039f39e28c415bc22d7d15fa7
Laura Neves <lauracmneves@gmail.com>
2022-10-29 22:29:57 -0300

    make it better

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ import express from "express";
 import "express-async-errors";
 import router from "./route/index.js";
 
-// import recommendationRouter from "./routers/recommendationRouter.js";
 import dotenv from "dotenv"
-// import e2eRouter from "./routers/e2eRouter.js";
 dotenv.config()
 
 const app = express();
@@ -13,14 +11,5 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
-
-
-// app.use("/recommendations", recommendationRouter);
-// app.use(errorHandlerMiddleware);
-
-// if(process.env.NODE_ENV==="test"){
-//     app.use(e2eRouter)
-//     console.log("Estou no ambiente de testes")
-// }
 
 export default app;
